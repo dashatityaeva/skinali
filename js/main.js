e42159422b70af126fbc9e95f140bba068ecd11a
Daria <tityaevadash@gmail.com>
2021-11-11 17:20:04 +0300

    gsap

--- js/main.js
+++ js/main.js
@@ -10,7 +10,9 @@ $(window).load(function () {
         prevArrow: $('.arrow-left'),
         nextArrow: $('.arrow-right')
     });
+    
     $('.menu-button').on('click', function() {
+        console.log( $('.menu-button'));
         $('.menu').toggleClass('menu_active');
     });
 
@@ -27,9 +29,123 @@ $(window).load(function () {
 
     $("a[href^='#']").click(function (){
         var _href=$(this).attr("href");
-        $("html, body").animate({scrollTop: $(_href).offset().top -200+ "px"});
+        $("html, body").animate({scrollBottom: $(_href).offset().bottom -200+ "px"});
         return false;
     });
 
     $('input[type="tel"]').mask("+7 (999) 999-99-99");
 });
+
+//gsap
+
+//navbar
+var navbarTl = gsap.timeline({duration: 0.3});
+navbarTl.fromTo(".menu-button", {x: -100, opacity: 0}, {x: 0, opacity: 1})
+    .fromTo(".logo", {y: -200, opacity: 0}, {y: 0, rotationX: 360, opacity: 1}, "-=0.2")
+    .fromTo(".navbar__mobile", {y: -200, opacity: 0}, {y: 0, rotationX: 360, opacity: 1})
+    .fromTo(".navbar__landline", {y: -200, opacity: 0}, {y: 0, rotationX: 360, opacity: 1})
+    .fromTo(".button-o", {x: 100, opacity: 0}, {x: 0, opacity: 1});
+
+//hero
+var heroTl = gsap.timeline({duration: 0.2});
+heroTl.fromTo(".hero", {opacity: 0, scale: 1.2}, { opacity: 1, scale: 1, ease: "circ.out"})
+heroTl.fromTo(".hero__title", {opacity: 0, scale: 1.2, x: -20, y: -20}, { opacity: 1, scale: 1,  x: 0, y: 0, ease: "power2.out"})
+heroTl.fromTo(".hero__subtitle", {opacity: 0, scale: 1.2, x: -20,}, { opacity: 1, scale: 1, x: 0, ease: "power2.out"})
+heroTl.fromTo(".hero__button", {opacity: 0, scale: 1.2}, { opacity: 1, scale: 1, ease: "power2.out"})
+heroTl.fromTo("#offer1", {opacity: 0, y: 100}, { opacity: 1, y: 0})
+
+//features
+gsap.utils.toArray(".features-block__item").forEach((item, index ) => {
+    let featuresTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: item,
+            start: "top 20%",
+            end: "top 20px",
+            toggleActions: "restart none reverse none",
+            // markers: true,
+            // scrub: true
+            pin: true,
+            pinSpacing: false,
+        },
+        duration: 0.9
+    });
+
+    featuresTl
+    .to(item, {x: 100, rotationY: 360,})
+    .to(item, {x: 0, })
+    item.animation = featuresTl;
+})
+
+//catalog
+gsap.utils.toArray(".catalog__card").forEach((item, index ) => {
+    let featuresTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: item,
+            start: "top 10%",
+            end: "top 50px",
+            toggleActions: "restart none reverse none",
+            scrub: true,
+        },
+        duration: 2
+    });
+
+    featuresTl
+    .to(item, { opacity: 1})
+    .to(item, { opacity: 0})
+    item.animation = featuresTl;
+})
+//blog
+gsap.to(".blog-card", { opacity: 0, x: -10, y: -10, duration: 0});
+gsap.utils.toArray(".blog-card").forEach((item, index ) => {
+
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".blog__title",
+            start: "top 50%",
+        },
+        duration: index / 2
+    });
+
+    tl.to(item, { opacity: 1,  x: 0, y: 0})
+      item.animation = tl;
+})
+
+//price
+
+//steps ??
+gsap.to(".steps", {
+    scrollTrigger: {
+        trigger: ".steps",
+        start: "top top",
+        end: "+=150px",
+        pinSpacing: false,
+        pin: true,
+        // markers: true,
+    },
+});
+
+
+//reviews
+gsap.to(".reviews-slider__item", { scale: 0.5, duration: 0});
+$(".reviews-slider__item").mouseenter(function(){
+    $(".reviews-slider__item").each(function (index, box){
+        let tl = new TimelineLite();
+        tl.
+          to(box, 0.1, {
+            duration: 1,
+            scale: 1,
+            ease: "linear",
+        })
+        box.animation = tl;
+      })
+  })
+$(".reviews-slider__item").mouseleave(function(){
+    gsap.to(".reviews-slider__item", { scale: 0.6, duration: 1, ease: "linear",});
+})
+
+
+//offer
+
+
+//factory
+
